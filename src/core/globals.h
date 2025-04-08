ed0fa07f65d947ed0199e191429d8c490e6534e1
saad <45915273+rootexpm@users.noreply.github.com>
2025-04-08 15:13:24 +0100

    Revert "add menu"
    This reverts commit 2212429723ceac2154674202a403876ec0f229fe.

--- src/core/globals.h
+++ src/core/globals.h
@@ -1,32 +1,10 @@
 #pragma once
-#include "../valve/centity.h"
 
-namespace globals {
+class CEntity;
+namespace globals
+{
 	inline CEntity* localPlayer = nullptr;
 
 	// update the local player pointer
 	void UpdateLocalPlayer() noexcept;
-
-	struct Config {
-		struct {
-			bool enabled = false;
-			float fov = 90.0f;
-			float smooth = 1.0f;
-		} aimbot;
-
-		struct {
-			bool esp_enabled = false;
-			bool box_esp = false;
-			bool name_esp = false;
-			bool health_esp = false;
-		} visuals;
-
-		struct {
-			bool bhop = false;
-			bool no_flash = false;
-			bool radar_hack = false;
-		} misc;
-	};
-
-	inline Config config;
 }
